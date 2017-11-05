23b51897241d15c9b9493b6a6876e415f32193e4
luis castro <langelolcastro@gmail.com>
2017-11-05 11:15:59 -0200

    adjusted app.js

--- app.js
+++ app.js
@@ -48,9 +48,10 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
+/*
 app.listen(process.env.PORT || 80, function() {
   console.log('server started');
 });
+*/
 
 module.exports = app;
