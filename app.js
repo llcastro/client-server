b028762d13db737d6edd1e7611a6911da2001513
luis castro <langelolcastro@gmail.com>
2017-11-16 19:29:44 -0200

    add methods

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Allow-Methods", "GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-Width, Content-Type, Accept");
   next();
 });
